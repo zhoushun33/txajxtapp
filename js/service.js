72c0597d59e24bfcb38f7deca9def9a53af77156
zhoushun33 <1071207477@qq.com>
2018-07-16 10:29:28 +0800

    2018年7月16日10:28:40 同欣同步备份

---
+++ js/service.js
@@ -0,0 +1,28 @@
+/*
+ *服务器参数
+ * 参与开发的人员有：jinghenglin/shaohangwu/jianhanghe/hongfazhang
+ * 维护：jinghenglin/jiajunlin
+ * 2016-3-31
+ *
+ * ********* my_js_cgwztmck.js中的第596行和第623行中IP要换，和url的IP是一样的
+ * 			my_plus/my_project_info.html中566行中IP要换，和url的IP是一样的
+ *		
+ */
+//var url='http://192.168.155.1:8083/hxadmin/hxajxt/';
+//var url='http://192.168.43.43:8082/hxadmin2/hxajxt/';
+//var url='http://192.168.0.6:8081/hxajxt/';
+//var url='http://192.168.0.30:80/hxadmin/hxajxt/';
+//var url='http://202.105.179.167:8080/hxajxtapp/hxajxt/';
+//var url='http://192.168.0.191:8082/sgajxtpc/sgajxtcs/'; //总公司测试
+var url='http://202.105.179.167:8080/txajxtappnew/txajxtcs/';
+var urlx='http://202.105.179.167:8080/txajxtappnew/';
+//var url='http://192.168.0.183:8081/sgajxt/sgajxtcs/';
+//var urlx='http://192.168.0.183:8081/sgajxt/';
+var servername='root';
+var serverpassword='123456';
+//项目基本信息中用于预览图片，结合PC端
+//var gcmcxx='http://192.168.43.43:8082/hxadmin2/ProjectPlus/';
+//var gcmcxx='http://192.168.0.107:80/hxadmin/ProjectPlus/';
+//var gcmcxx='http://192.168.0.13:8083/hxadmin/ProjectPlus/';
+//var gcmcxx='http://202.105.179.167:8080/hxajxtapp/hxajxt/';
+var gcmcxx='http://202.105.179.167/txajxtapp/txajxtcs/'; //总公司测试
